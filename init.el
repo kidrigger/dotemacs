0282d97a4078163461461d0d1a06f44a03925948
Anish Bhobe <anishbhobe@hotmail.com>
2024-05-03 01:01:28 +0200

    init.el follows emacs style of comments.

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
-;; kidrigger's config
+;;; package -- Summary
+;;; kidrigger's config
 
+;;; Commentary:
+;;; Just the basic config for a simple man.
+
+;;; Code:
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
